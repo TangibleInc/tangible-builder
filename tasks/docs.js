86e1984030ec50090777a8457906abf25e429cad
eliot-akira <me@eliotakira.com>
2020-01-01 20:32:26 +0100

    Docs: Extract class/function definition following a DocBlock

--- tasks/docs.js
+++ tasks/docs.js
@@ -14,20 +14,29 @@ const ltrimRe = /^\s*/
 const rtrimRe = /\s*$/
 const ltrimNewlineRe = /^(\r?\n)+/
 const multilineRe = /(?:^|\r?\n) *(@[^\r\n]*?) *\r?\n *(?![^@\r\n]*\/\/[^]*)([^@\r\n\s][^@\r\n]+?) *\r?\n/g
+const newlineRe = /(?:\r?\n)/g
 
 const stringStartRe = /(\r?\n|^)\s*\*\s?/g
-const propertyRe = /(?:^|\r?\n)\s*@(\S+)\s*([^\r\n]*)/g
+const propertyRe = /(?:^|\r?\n)*@(\S+)\s*([^\*@\r\n]*)/g
+const propertyStartRe = /\s*@(\S+)/
 const propertyValuesRe = /(\S+)\s*(\S+)*([^\r\n]*)/
 const returnTypeRe = /(\S+)*([^\r\n]*)/
 const startCurlyRe = /^{/
 const endCurlyRe = /}$/
+const repeatedSpaceRe = /(\s+)/g
+
+const definitionClassRe = /^\s*[c|C]lass\s*(\S+)\s*(extends\s*\S+)?\s*(implements\s*(([^\s{\/],?\s*)+))?\s*{/
+const definitionFuncRe = /^\s*(public|private|protected|static)?\s*function\s*(\S+)\s*\((\s*([^\s{\/],?\s*)+)?\)\s*{/
+// const definitionPHPAnonFuncRe = //
+// const definitionJSAnonFuncRe = //
+// const definitionCSSRe = //
 
 const detectNewline = string => {
   if (typeof string !== 'string') {
     throw new TypeError('Expected a string')
   }
 
-  const newlines = string.match(/(?:\r?\n)/g) || []
+  const newlines = string.match(newlineRe) || []
 
   if (newlines.length === 0) {
     return
@@ -44,7 +53,35 @@ function extract(contents) {
   let match
   let matches = []
   while ((match = docblockRe.exec(contents))) {
-    matches.push(match[0])
+
+    let block = match[0]
+
+    // Extract class/function definition following a DocBlock
+
+    let index = match.index + block.length
+    let rest = contents.substr(index)
+
+    let definition
+    let definitionType
+    let defMatch
+
+    if ((defMatch = definitionClassRe.exec(rest)) && defMatch[1]) {
+      definitionType = 'class'
+      definition = defMatch[1]
+      if (defMatch[2]) definition += ' '+defMatch[2].replace(repeatedSpaceRe, ' ').replace(rtrimRe, '')
+      if (defMatch[3]) definition += ' '+defMatch[3].replace(repeatedSpaceRe, ' ').replace(rtrimRe, '')
+    } else if ((defMatch = definitionFuncRe.exec(rest)) && defMatch[2]) {
+      definitionType = (defMatch[1] ? defMatch[1]+' ' : '')+'function'
+      definition = defMatch[2]+(
+        defMatch[3] ? '('+defMatch[3]+')' : '()'
+      )
+    }
+
+    if (definition) {
+      block = block.replace(commentEndRe, '\n * @definitionType '+definitionType+'\n * @definition '+definition+'\n */')
+    }
+
+    matches.push(block)
   }
   return matches.map(match => match.replace(ltrimRe, ''))
 }
@@ -74,27 +111,32 @@ function parseDocblock(docblock) {
 
   if (pos === -1) {
     // Only line is title
-    if (line[0]!=='@') {
+    if (!propertyStartRe.exec(line)) {
       result.title = rest
       return result
     }
   } else {
     do {
 
-      const line = rest.substr(0, pos) // Every line is already left/right trimmed
+      const line = rest.substr(0, pos).replace(stringStartRe, '')
 
       if (line) {
         // First property encountered
-        if (line[0]==='@') break
+        if (propertyStartRe.exec(line)) break
 
         if (!result.title) {
-          result.title = line
+          result.title = line.replace(ltrimRe, '').replace(rtrimRe, '')
         } else if (!result.description) {
           result.description = line
         } else {
           // Keep adding to description
           result.description += "\n" + line
         }
+      }
+
+      if (typeof result.description !== 'undefined') {
+        result.description = result.description.replace(ltrimRe, '').replace(rtrimRe, '')
+        if (!result.description) delete result.description
       }
 
       rest = rest.substr(pos + 1)
@@ -124,13 +166,15 @@ function parseDocblock(docblock) {
         description: (valMatch[3] || '').replace(ltrimRe, '').replace(rtrimRe, ''),
       }
 
+      if (!prop.description) delete prop.description
+
       result[key].push(prop)
       continue
     }
 
-    if (key==='return' || key==='returns') {
+    if (key==='return' || key==='returns' || key==='var') {
 
-      // Return: Type, description
+      // Return, Variable: Type, description
 
       const valMatch = returnTypeRe.exec(value)
       if (valMatch) {
@@ -139,7 +183,11 @@ function parseDocblock(docblock) {
           description: (valMatch[2] || '').replace(ltrimRe, '').replace(rtrimRe, ''),
         }
 
-        result['return'] = prop
+        if (!prop.description) delete prop.description
+
+        result[
+          key==='returns' ? 'return' : key
+        ] = prop
       }
       continue
     }
