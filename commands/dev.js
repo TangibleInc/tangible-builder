02aa14fe46592587bca96f649aafa0c6cb0e9418
eliot-akira <me@eliotakira.com>
2020-02-22 21:10:43 +0100

    Add build task "babel" to transpile for non-browser target, such as server-side and library

--- commands/dev.js
+++ commands/dev.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const watch = require('gulp-watch')
 
 module.exports = async function devCommand(config) {
@@ -37,8 +38,21 @@ module.exports = async function devCommand(config) {
 
     console.log(chalk.blue('watch'), task.watch)
 
-    // HTML task watches and individually compiles
+    // HTML and Babel tasks watch and individually compile
     if (task.task==='html') continue
+    if (task.task==='babel') {
+      watch(task.watch, (f) => {
+        const src = f.history[0]
+        if (!src) return // TODO: Handle remove file?
+        const srcRelativeDir = path.relative(f.base, path.dirname(f.history[0]))
+        const dest = path.join(task.dest, srcRelativeDir)
+        runTaskAction({
+          ...task,
+          src, dest
+        }).catch(e => e && console.error(e.message))
+      })
+      continue
+    }
 
     const isCss = task.task==='sass'
 
---
+++ tasks/babel.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const createBabelConfig = require('../config/babel')
+
+module.exports = function babelTask(config) {
+
+  const {
+    task: { src, dest, root: rootDirs = [] },
+    isDev = false,
+    toRelative, chalk, fileExists,
+  } = config
+
+  const babelConfig = createBabelConfig(config)
+  const extensions = ['.js', '.jsx', '.ts', '.tsx', '.json']
+
+  return new Promise((resolve, reject) => {
+
+    return gulp.src(src, {
+      read: true,
+      allowEmpty: true
+    })
+      .pipe(babel(babelConfig))
+      .pipe(gulp.dest(dest))
+      .on('error', function(e) {
+        if (e.message) console.error(chalk.red('babel'), e.message)
+        this.emit('end')
+        reject()
+      })
+      .on('end', () => {
+        console.log(chalk.green('babel'), `${toRelative(src)} -> ${toRelative(dest)}`)
+        resolve()
+      })
+  })
+}
